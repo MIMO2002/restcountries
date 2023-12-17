b04de8d76f03c5202c145d6aff746b96242ed2e3
Sethet <sethet188@gmail.com>
2023-12-17 12:53:28 +0700

    Display [Flag, Official Name, 2 char Country Code, 3 char Country Code, Native Name, Alternative Name, Calling Codes]

--- src/components/Home.js
+++ src/components/Home.js
@@ -28,21 +28,40 @@ function Home() {
     return ( 
         <div>
             {loading ? 'Loading...' : (
-                <tbody>
-                    {countries.map((country) => {
-                        return (
-                            <tr key={country.cca2}>
-                                <td>
-                                    <img src={country.flags.png} alt="Flag" width="32" />
-                                </td>
-                                <td>{country.name.official}</td>
-                                <td>{country.cca2}</td>
-                                <td>{country.cca3}</td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-                )}
+                <table>
+                    <tbody>
+                        {countries.map((country) => {
+
+                            return (
+                                <tr key={country.cca2}>
+                                    <td className='Flag-img'>
+                                        <img src={country.flags.png} alt="Flag" width="32" />
+                                    </td>
+                                    <td className='Official-name'>{country.name.official}</td>
+                                    <td>{country.cca2}</td>
+                                    <td>{country.cca3}</td>
+                                    <td className='Native-name'>
+                                        {country.name.nativeName && Object.values(country.name.nativeName).map((nativeName, index) => (
+                                            <React.Fragment key={index}>
+                                                {nativeName.official}, {nativeName.common} {index < Object.values(country.name.nativeName).length - 1 && ', '}
+                                            </React.Fragment>
+                                        ))}
+                                    </td>
+                                    <td>{country.altSpellings ? country.altSpellings.join(', ') : ''}</td>
+                                    <td>
+                                        root: {country.idd.root},
+                                        suffixes: {country.idd.suffixes && country.idd.suffixes.map((suffix, index) => (
+                                            <React.Fragment key={index}>
+                                                {suffix}{index < country.idd.suffixes.length - 1 && ', '}
+                                            </React.Fragment>
+                                        ))}
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            )}  
             {error ? error : null}
         </div>
      );
